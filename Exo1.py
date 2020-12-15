fc40c5a63b8940704fe2e4068acb0d63afab4f32
FlorentMonnet <57752236+FlorentMonnet@users.noreply.github.com>
2020-12-15 11:19:05 +0100

    Update Exo1.py

--- Exo1.py
+++ Exo1.py
@@ -2,7 +2,7 @@
 #On définie f donct il faudra calculer l'intégrale
 def f(x):
     return x
-#On définit integ_rumberg, je n'ai pas uitilisé epsilon mais le nomnbre d'intervalles
+#On définit integ_rumberg, je n'ai pas uitilisé epsilon mais le nombre d'intervalle
 def  integ_romberg(f, a, b,nbInter=30):
     #On a donc h =(b-a)/nbInter
     h=(b-a)/nbInter
@@ -12,7 +12,7 @@ def  integ_romberg(f, a, b,nbInter=30):
     #somme de 1 à nbInter, qui ajoute f(a+i*h)
     for i in range(nbInter):
         somme+=f(a+i*h)
-    #resultat final = h * f(a)+f(ab)/2 + la somme précédent * h
+    #resultat final = h * f(a)+f(ab)/2 + la somme précédente * h
     return result +somme*h
 #Pn affiche le résultat de l'intégrale de f de 0 à 1
 print(integ_romberg(f,0,1))
