9880d3d87621f4f8f7a6140773c3a685166b2a02
FlorentMonnet <57752236+FlorentMonnet@users.noreply.github.com>
2020-12-15 11:03:11 +0100

    Add files via upload
    J'ai eu un problème avec Jupyter, je mets donc des .py

---
+++ Exo1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#On définie f donct il faudra calculer l'intégrale
+def f(x):
+    return x
+#On définit integ_rumberg, je n'ai pas uitilisé epsilon mais le nomnbre d'intervalles
+def  integ_romberg(f, a, b,nbInter=30):
+    #On a donc h =(b-a)/nbInter
+    h=(b-a)/nbInter
+    #result h * f(a)+f(ab)/2
+    result=h*(f(a)+f(b))/2
+    somme=0
+    #somme de 1 à nbInter, qui ajoute f(a+i*h)
+    for i in range(nbInter):
+        somme+=f(a+i*h)
+    #resultat final = h * f(a)+f(ab)/2 + la somme précédent * h
+    return result +somme*h
+#Pn affiche le résultat de l'intégrale de f de 0 à 1
+print(integ_romberg(f,0,1))
---
+++ Exo3.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 10:06:51 2020
+
+@author: monnetf
+"""
+import numpy as np
+import math
+class Ville:
+    #On initialise aucune destination
+    def __init__(self):
+        self.destination=[]
+    
+    def getDestination(self):
+        return self.destination
+    
+    def setDestination(self,Xa,Ya):
+        self.destination=np.append(self.destination,[Xa,Ya])
+    
+    def aleatoire(self,n):
+        #On parcoure notre boucle grace au nombre fournis par l'utilisateur
+        for i in range(n):
+            #On ajoute deux noms aléatoires entre 0 et 50 à destination pour rajouter une destination
+            self.destination=np.append(self.destination,[np.random.randint(50),np.random.randint(50)])
+    def nb_trajet(self):
+        #On recupere le nombre de valeurs dans destination puis on divise par 2 pour avoir le nombre de destination
+        n = self.destination.size/2
+        #On retourne (n-1)!/2
+        return math.factorial(n-1)/2
+    def distance(self,i,j):
+        #On retourne la distance manathan comme dans m'énoncé
+        return abs(i[1]-i[0])+abs(j[1]-j[0])
+#On créé une ville avec 4 destinations            
+ville=Ville()
+ville.aleatoire(4)
+print(ville.distance([4,2],(1,1)))
+#On affiche le nombre de trajet de la ville
+print(ville.nb_trajet())
+#Je n'ai pas réussi à append des tableaux de deux dimensions donc il y a qu'une dimension pour toutes les destinations .. 
+print(ville.getDestination())
